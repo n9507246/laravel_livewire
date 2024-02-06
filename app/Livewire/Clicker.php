285c7b3a523c2e72ed0cbd09a66c815112a6cd57
Nikita Balashov <noWorkingEmail@mail.ru>
2024-02-06 15:42:20 +0500

    форма создания пользователей

--- app/Livewire/Clicker.php
+++ app/Livewire/Clicker.php
@@ -6,17 +6,23 @@ use Livewire\Component;
 
 class Clicker extends Component
 {
+    public $name;
+    public $email;
+    public $password;
+    
+    
     public function createNewUser(){
-        \App\Models\User::factory()->create([
-            'name' => 'New test User',
-            'email' => 'test2@example.com',
+        \App\Models\User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => $this->password
         ]);
     }
 
     public function render()
     {
         return view('livewire.clicker', [
-            'users' => \App\Models\User::orderBy('id', 'desc')->get()
+            'users' => \App\Models\User::orderBy('id', 'desc')->limit(10)->get()
         ]);
     }
 }
--- resources/views/livewire/clicker.blade.php
+++ resources/views/livewire/clicker.blade.php
@@ -1,11 +1,41 @@
 <div>
-    <button wire:click='createNewUser'>Create new user</button>
-
-    <ul>
-        @foreach($users as $user)
-            <li style="margin-bottom: 10px">
-                Name: {{ $user->name }}. Email: {{$user->email}}
-            </li>
-        @endforeach
-    </ul>
+    <div class="m-auto w-75 card mb-4 p-4 shadow">
+        <form wire:submit='createNewUser' >
+            <div class="mb-3">
+              <label for="name" class="form-label">Name</label>
+              <input wire:model='name' type="text" class="form-control" id="name" name='name' aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input wire:model='email' type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+              <label for="password" class="form-label">Password</label>
+              <input wire:model='password' type="password" class="form-control" id="password" name="password">
+            </div>
+            <button class="btn btn-success">Submit</button>
+        </form>
+    </div>    
+    
+    <div class="m-auto w-75 card mb-4 p-1 shadow ">
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Created at</th>
+            </tr>
+            </thead>
+            <tbody>
+                @foreach($users as $user)
+                    <tr>
+                        <td>{{ $user->name }}</td>
+                        <td>{{$user->email}}</td>
+                        <td>{{$user->created_at}}</td>    
+                    </tr>
+                @endforeach
+            
+            </tbody>
+        </table>
+    </div>
 </div>
